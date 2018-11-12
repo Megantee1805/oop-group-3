b3b0739a32cf8caeeecddeafbfea488de67b2668
Megantee1805 <44191183+Megantee1805@users.noreply.github.com>
2018-11-12 10:01:59 +0800

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/index')
+@app.route('/')
 def hello_world():
     return render_template("index.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
 
 if __name__ == '__main__':
