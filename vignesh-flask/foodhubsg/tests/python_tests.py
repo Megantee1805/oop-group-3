326172182a16c64d849f2be9e465c5b884ac7a92
Vignesh <bala12rupesh@gmail.com>
2018-12-09 13:25:39 +0800

    The food history now has a working calorie counter
    That took way longer than I expected but I got it to work. There's nothing else major I need to add to the food history I'm just going to move on to other functions.

--- vignesh-flask/foodhubsg/tests/python_tests.py
+++ vignesh-flask/foodhubsg/tests/python_tests.py
@@ -1,11 +1,11 @@
-# from flask import (
-#     Blueprint, flash, g, redirect, render_template, request, url_for
-# )
-# from werkzeug.exceptions import abort
-#
-# from foodhubsg.auth import login_required
-# from foodhubsg.db import get_db
-# from foodhubsg.classes import *
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from foodhubsg.auth import login_required
+from foodhubsg.db import get_db
+from foodhubsg.classes import *
 
 
 def remove_duplicates(values):
@@ -17,32 +17,38 @@ def remove_duplicates(values):
             seen.add(value)
     return output
 
+@bp.route('/')
+@login_required
+def index():
+    """Show all recent meals, most recent first."""
+    db = get_db()
+    food_items = db.execute(
+        'SELECT f.id, creator_id, food_name, created, calories, food_code, email'
+        ' FROM food_entry'
+        ' ORDER BY datetime(created) DESC',
+    ).fetchall()
 
-# db = get_db()
-# food_items = db.execute(
-#     'SELECT f.id, creator_id, food_name, created, calories, food_code, email'
-#     ' FROM food_entry f JOIN user u ON f.creator_id = u.id'
-#     ' WHERE f.creator_id = ?',
-#     (g.user['id'],)
-# ).fetchall()
+    all_dates = []
+    food_dates = []
+    calories_list = []
 
-
-all_dates = []
-food_dates = []
-
-
-for food in food_items:
-    food_date = food['created'].strftime('%Y-%m-%d')
-    all_dates.append(food_date)
-remove_duplicates(all_dates)
-
-for food_date in all_dates:
-    current_date_food = []
     for food in food_items:
-        if food['created'].strftime('%Y-%m-%d') == food_date:
-            current_date_food.append(food)
-        else:
-            continue
-        food_dates.append(current_date_food)
+        food_date = food['created'].strftime('%d-%m-%y')
+        all_dates.append(food_date)
+    all_dates = remove_duplicates(all_dates)
 
-print(food_dates)
+    for date in all_dates:
+        current_date_food = []
+        current_date_calories = []
+
+        for food in food_items:
+            if date == food['created'].strftime('%d-%m-%y'):
+                current_date_food.append(food)
+                current_date_calories.append(food['calories'])
+            else:
+                continue
+        food_dates.append(current_date_food)
+        current_date_calories = sum(current_date_calories)
+        calories_list.append(current_date_calories)
+
+    return render_template('food/index.html', food_dates=food_dates, all_dates=all_dates, calories_list=calories_list)
