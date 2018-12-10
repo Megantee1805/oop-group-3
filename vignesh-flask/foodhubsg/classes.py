d0becf884f14335f0ffcdb43c702091cb8987cd4
Megan Tee <183678E@mymail.nyp.edu.sg>
2018-12-10 22:04:06 +0800

    Update classes.py

--- vignesh-flask/foodhubsg/classes.py
+++ vignesh-flask/foodhubsg/classes.py
@@ -78,5 +78,4 @@ food_list = [a00001, a00002, b00001, b00002]
 
 sen01 = Vendor("sen01", "McDonald's", 1207, "Sengkang", "This stuff will literally kill you and you pay us for it.", [a00001, a00002])
 sen02 = Vendor("sen02", "Western Delight - Sengkang Kopitiam", 979, "Sengkang", "Come buy overpriced frozen food!", [b00001, b00002])
-sen03 = Vendor("sen03", "The Lawn", 700, "AMK HUB", "It looks nice, but will cause problems in the long run")
 vendor_list = [sen01, sen02]
