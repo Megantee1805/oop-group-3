b5685635013a32259e8e51112c3b5c372e754c21
Megan Tee <183678E@mymail.nyp.edu.sg>
2018-12-11 16:58:30 +0800

    add

--- vignesh-flask/foodhubsg/vendors.py
+++ vignesh-flask/foodhubsg/vendors.py
@@ -80,9 +80,13 @@ def food_journal():
 @login_required
 @bp.route('/vendors')
 def vendors():
-    for i in vendor_list:
-        vendors = []
-        vendors.append(i)
-        return render_template("food/vendor.html")
+    for location in locations:
+        for vendors in location:
+            name = vendors.get_name()
+            location = vendors.get_location()
+            calories = vendors.get_average_calories()
+            description = vendors.get_description()
+            return render_template("food/vendor.html", vendors=vendors, name=name, location=location, calories=calories,
+                                   description=description)
 
 
