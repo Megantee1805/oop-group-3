e0deeba143e73a5ecac9d33723b854afc47b4e5e
Megan Tee <183678E@mymail.nyp.edu.sg>
2018-12-11 18:38:01 +0800

    settings

--- vignesh-flask/foodhubsg/classes.py
+++ vignesh-flask/foodhubsg/classes.py
@@ -71,15 +71,20 @@ class Vendor:
 
 a00001 = Food("a00001", "Chicken Rice", 673)
 a00002 = Food("a00002", "Fried Rice", 840)
+a00003 = Food("a00003", "Aglio Olio", 600)
+a00004 = Food("a00004", "Cesear Salad", 540)
 b00001 = Food("b00001", "McSpicy", 752)
 b00002 = Food("b00002", "Straight-Up Blood", 109)
+b00003 = Food("b00003", "Teriyaki Bento", 631)
+b00004 = Food("b00004", "Karage Ramen", 570)
+
 
 food_list = [a00001, a00002, b00001, b00002]
 
 sen01 = Vendor("sen01", "McDonald's", 1207, "Sengkang", "This stuff will literally kill you and you pay us for it.", [a00001, a00002])
 sen02 = Vendor("sen02", "Western Delight - Sengkang Kopitiam", 979, "Sengkang", "Come buy overpriced frozen food!", [b00001, b00002])
-amk01 = Vendor("amk01", "The Lawn", 798, "AMK Hub", "The ambience is nice but no more than that", [])
-amk02 = Vendor("amk02", "Lean Bento", 699, "AMK Hub", "The food is decently healthy but still way too sweet", [])
+amk01 = Vendor("amk01", "The Lawn", 798, "AMK Hub", "The ambience is nice but no more than that", [a00003, a00004])
+amk02 = Vendor("amk02", "Lean Bento", 699, "AMK Hub", "The food is decently healthy but still way too sweet", [b00003, b00004])
 amkhub = [amk01, amk02]
 sengkang = [sen01, sen02]
 locations = [amkhub, sengkang]
