0e99543c10158766be217f2d12535a7f8df961d0
Megan Tee <183678E@mymail.nyp.edu.sg>
2018-12-26 22:04:56 +0800

    email

--- vignesh-flask/foodhubsg/__init__.py
+++ vignesh-flask/foodhubsg/__init__.py
@@ -34,4 +34,10 @@ def create_app(test_config=None):
 
     app.add_url_rule('/', endpoint='index')
 
+    from flask_mail import Mail
+    mail = Mail()
+
+    app = Flask(__name__)
+    mail.init_app(app)
+
     return app
